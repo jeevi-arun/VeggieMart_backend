88de69ff3b9f3cbdbc9ed8b8439823dc2621232c
Jeevi-arun <jeevithasen2000@gmail.com>
2025-10-07 15:47:21 +0530

    Added Vercel domain to CORS and connected to MongoDB Atlas

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ const PORT = process.env.PORT || 5000;
 
 
 app.use(cors({
-  origin: "http://localhost:5173", // change to your frontend URL when deployed
+  origin: "http://localhost:5173",  "https://veggie-mart-frontend.vercel.app",
   credentials: true,
 }));
 app.use(express.json());
 
 // --- MongoDB Connection ---
-mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/shopDB", {
+mongoose.connect(process.env.MONGO_URI , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
